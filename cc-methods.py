b4acf6a6065af6dc2b9974c60b62a4008269c864
Matthew Robichaud <m5robich@uwaterloo.ca>
2021-03-17 13:35:52 -0400

    Added Steeped Disk Model

--- cc-methods.py
+++ cc-methods.py
@@ -62,4 +62,33 @@ def volume_24well(r_i,alpha,hmax):
         return (hmax - (r-16.2/2)/math.tan(beta))*2*math.pi*r
     
     return quad(int1,0,r_i)[0] + quad(int2,r_i,16.2/2)[0] + quad(int3, 16.2/2,r_omax/2)[0]
+
+# accepts array of radii from centre of rotation and returns shear rates
+# omega(r)/h
+# omega is given in RPM for ease of use
+# r and h must be in same units
+def disk_Shear(omega,r,h):
+    return rad(omega)*r/h
     
+# accepts array of radii from centre of rotation and returns shear rates
+# omega(r)/h
+# omega is given in RPM for ease of use
+# r and h must be in same units
+# SDM
+# h1 is inner
+# ratio: of surface areas, zone 1 : zone 2
+def SDM_Shear(omega,r,h1,h2,frac):
+    
+    
+    ratio = frac / (1 - frac)
+    
+    r_star = math.sqrt(4*ratio + max(r)**2) / math.sqrt(ratio+1)
+    
+    out = []
+    
+    for rs in r: 
+        if (rs < r_star):
+            out.append(rad(omega)*rs/h1)
+        else: 
+            out.append(rad(omega)*rs/h2)
+    return np.array(out)
